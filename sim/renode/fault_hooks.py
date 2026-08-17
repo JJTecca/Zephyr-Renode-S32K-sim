2cf42b026fc8d45a602b4fae8958cc71c517876d
JJTecca <maiorcristian123@gmail.com>
2026-08-17 21:00:29 +0300

    [HOTFIX] --fault deadline_miss

--- sim/renode/fault_hooks.py
+++ sim/renode/fault_hooks.py
@@ -3,19 +3,19 @@ def _bus_addr():
     #renode type of variable
     return bus, bus.GetSymbolAddress("sdv_fault_ctl")
 
-def inject_memory_leak(machine_name, rate):
+def mc_nject_memory_leak(machine_name, rate):
     bus, a = _bus_addr()
     bus.WriteDoubleWord(a,     0xFA17C0DE)   # magic -> armed
     bus.WriteDoubleWord(a + 4, int(rate))    # leak_bytes_per_tick
     bus.WriteDoubleWord(a + 8, 0)            # timing off
 
-def inject_busy_spin(machine_name, us):
+def mc_inject_busy_spin(machine_name, us):
     bus, a = _bus_addr()
     bus.WriteDoubleWord(a,     0xFA17C0DE)   # magic -> armed
     bus.WriteDoubleWord(a + 4, 0)            # leak off
     bus.WriteDoubleWord(a + 8, int(us))      # busy_spin_us ramp per tick
 
-def clear_faults(machine_name):
+def mc_clear_faults(machine_name):
     bus, a = _bus_addr()
     bus.WriteDoubleWord(a,     0x0)          # magic = 0 -> disarmed (heals)
     bus.WriteDoubleWord(a + 4, 0)
