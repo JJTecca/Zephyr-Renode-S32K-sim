e1acd31729bcd3213346680f7842470233a12fe4
JJTecca <maiorcristian123@gmail.com>
2026-09-04 16:23:33 +0300

    [FEATURE] 4x K1 elfs

--- scripts/s32k1k3_build_os.ps1
+++ scripts/s32k1k3_build_os.ps1
@@ -11,9 +11,14 @@ Remove-Item -Recurse -Force build\k3_hub -ErrorAction SilentlyContinue
 Write-Output "Generating .elf file for s32k344"
 west build -b mr_canhubk3/s32k344 firmware\k3_hub -d build\k3_hub -p always
 
-Write-Output "Removing old configuration on K1 zonal"
+Write-Output "Removing old configuration on K1 nodes"
 Remove-Item -Recurse -Force build\k1_powertrain -ErrorAction SilentlyContinue
-Write-Output "Generating .elf file for s32k1"
-west build -b mr_canhubk3/s32k344 firmware\k1_edge -d build\k1_powertrain -p always
+Remove-Item -Recurse -Force build\k1_chassis    -ErrorAction SilentlyContinue
+Remove-Item -Recurse -Force build\k1_body       -ErrorAction SilentlyContinue
+Remove-Item -Recurse -Force build\k1_acoustic   -ErrorAction SilentlyContinue
+Write-Output "Generating 4x .elf file for s32k1"
+west build -b mr_canhubk3/s32k344 firmware\k1_edge -d build\k1_powertrain -p always -- -DCONFIG_SDV_NODE_ID=1
+west build -b mr_canhubk3/s32k344 firmware\k1_edge -d build\k1_chassis    -p always -- -DCONFIG_SDV_NODE_ID=2
+west build -b mr_canhubk3/s32k344 firmware\k1_edge -d build\k1_body       -p always -- -DCONFIG_SDV_NODE_ID=3
+west build -b mr_canhubk3/s32k344 firmware\k1_edge -d build\k1_acoustic   -p always -- -DCONFIG_SDV_NODE_ID=4
 pause
-
